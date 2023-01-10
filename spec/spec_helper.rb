7f2e5af965d42dc8ea6860ea69d27cb9548c7c00
Phil Pirozhkov <hello@fili.pp.ru>
2023-01-11 02:19:15 +0300

    Support Sidekiq 7 for local dev

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,6 @@ require 'active_support/core_ext/numeric/time'
 require 'active_support'
 require 'sidekiq'
 require 'rspec/enqueue_sidekiq_job'
+
+# We allow that for earlier versions and if users opt out of strict arg types
+Sidekiq.strict_args!(false) if Sidekiq::VERSION >= '7'
